329efc2b485efce2f3f9c923c0fd8e7573028a7c
Trim Bresilla <trim.bresilla@gmail.com>
2022-06-14 19:33:31 +0200

    FIX: runme thingy

--- run_me.sh
+++ run_me.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
-ls -sf /env/dot/.config/* $HOME/.config
+ln -sf /env/dot/.config/* $HOME/.config
 ln -sf /env/dot/.{aliases,bashrc,func,profile,startup,xinitrc,Xmodmap,zshrc} $HOME
